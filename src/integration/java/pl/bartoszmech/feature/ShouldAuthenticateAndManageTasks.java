92dbba678858d6394f79097e80342dbcab15bc02
FuuKowatty <miedzio.2000@gmail.com>
2023-12-15 11:25:59 +0100

    test: create test containers

---
+++ src/integration/java/pl/bartoszmech/feature/ShouldAuthenticateAndManageTasks.java
@@ -0,0 +1,36 @@
+package pl.bartoszmech.feature;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import pl.bartoszmech.BaseIntegrationTest;
+
+@Log4j2
+public class ShouldAuthenticateAndManageTasks extends BaseIntegrationTest {
+
+
+
+    @Test
+    public void should_authenticate_and_manage_tasks() {
+//        Step 1: Send and receive "Unauthorized" for every endpoint due to the absence of tokens.
+
+
+//        Step 2: Create an account.
+//        Step 3: Log in.
+//        Step 4: View a list of tasks (expecting an empty list).
+//        Step 5: Create a task.
+//        Step 6: View a list of tasks and receive them as a response.
+//        Step 7: Test endpoints to validate filters.
+//        Step 8: An employee attempts to create a user/task and receives an error.
+//        Step 9: A manager attempts to create a user and receives an error.
+//                Step 10: An employee displays only their tasks and receives a valid response.
+//                Step 11: An employee attempts to display all tasks and receives an error.
+//                Step 12: A manager tries to create a task and receives an "OK" response.
+//                Step 13: A manager attempts to create a user and receives an error.
+//                Step 14: A manager tries to delete a task and receives an "OK" response.
+//                Step 15: A manager tries to edit a task and receives an "OK" response.
+//                Step 16: Admin conducts CRUD operations on users.
+//        Step 17: An employee tries to mark a task as completed and receives an "OK" response.
+
+    }
+
+}
---
+++ src/main/java/pl/bartoszmech/TaskManager.java
@@ -0,0 +1,12 @@
+package pl.bartoszmech;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManager.class, args);
+    }
+}
