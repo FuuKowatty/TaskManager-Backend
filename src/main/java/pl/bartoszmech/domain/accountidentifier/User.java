5b765a0c10764bba656c5c68508f2086370eb9b5
FuuKowatty <miedzio.2000@gmail.com>
2023-12-16 19:03:29 +0100

    refactor: change user id to type Long instead of String

--- src/main/java/pl/bartoszmech/domain/accountidentifier/User.java
+++ src/main/java/pl/bartoszmech/domain/accountidentifier/User.java
@@ -1,15 +1,44 @@
 package pl.bartoszmech.domain.accountidentifier;
 
-import lombok.Builder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
 
-@Builder
-record User(
-        String id,
-        String firstName,
-        String lastName,
-        String email,
-        String password,
-        UserRoles role
-) {
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "users")
+@Getter
+class User{
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private UserRoles role;
+
+    public User(Long id, String firstName, String lastName, String email, String password, UserRoles role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String firstName, String lastName, String email, String password, UserRoles role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
 }
 
