34dd74e4c9ed1a80bb6fae7a0edad09adda09981
FuuKowatty <miedzio.2000@gmail.com>
2023-12-18 21:50:43 +0100

    feat: Implement new method to mark task as completed

--- src/main/java/pl/bartoszmech/domain/task/TaskRepository.java
+++ src/main/java/pl/bartoszmech/domain/task/TaskRepository.java
@@ -1,10 +1,17 @@
 package pl.bartoszmech.domain.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 interface TaskRepository extends JpaRepository<Task, Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE Task t SET t.isCompleted = true WHERE t.id = :taskId")
+    void markTaskAsCompleted(Long taskId);
 }
