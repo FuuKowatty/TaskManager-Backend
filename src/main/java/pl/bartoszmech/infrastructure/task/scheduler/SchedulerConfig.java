f70a38bf64a749c67e0699bf7e292278541a529e
FuuKowatty <miedzio.2000@gmail.com>
2024-01-01 23:29:33 +0100

    feat: add conditional enabled schedule and disable it for tests

--- src/main/java/pl/bartoszmech/infrastructure/task/scheduler/SchedulerConfig.java
+++ src/main/java/pl/bartoszmech/infrastructure/task/scheduler/SchedulerConfig.java
@@ -1,9 +1,11 @@
 package pl.bartoszmech.infrastructure.task.scheduler;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(name="task.schedule.enabled", matchIfMissing = true)
 public class SchedulerConfig {
 }
